c2050aae4d79dba2057de9a77e611f390d597103
Abinash Kumar Mishra <abmishra1234@gmail.com>
2024-05-17 00:22:05 +0530

    Improved Logs for inclusion of Filename , Function Name, and Line Number

--- Segment_Generator/logger.py
+++ Segment_Generator/logger.py
@@ -1,11 +1,26 @@
 import logging
+import os
+from datetime import datetime
 
 def setup_logger():
+    # Create logs directory if it does not exist
+    if not os.path.exists('logs'):
+        os.makedirs('logs')
+    
+    # Setting up the filename with date
+    date_str = datetime.now().strftime("%Y%m%d")
+    log_filename = f"logs/log_sg_{date_str}.log"
+    
+    # Precisely defined format to match your example
+    log_format = ('%(asctime)s - %(filename)s:%(funcName)s:%(lineno)d - '
+                  '%(name)s - %(levelname)s - %(message)s')
+
     logging.basicConfig(
         level=logging.INFO,
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        format=log_format,
+        datefmt='%Y-%m-%d %H:%M:%S',
         handlers=[
-            logging.FileHandler("segment_generator.log"),
+            logging.FileHandler(log_filename),
             logging.StreamHandler()
         ]
     )
